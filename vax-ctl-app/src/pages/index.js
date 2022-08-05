e3ec66fbb13a96aff1f68e39b5d9b962317d6e0d
Andrés Salazar <andres.salazar.d.18@gmail.com>
2022-08-04 23:47:44 -0500

    Layout component created

--- vax-ctl-app/src/pages/index.js
+++ vax-ctl-app/src/pages/index.js
@@ -1,18 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        HOME PAGE
-      </main>
-
-      <footer className={styles.footer}>
-        FOOTER
-      </footer>
-
-    </div>
+  return(
+    <Layout>
+      MAIN
+    </Layout>
   )
 }
